60c02688d3ed9b742ba7d63d5e0023b03b754582
poojithadasari20 <poojithadasari20@gmail.com>
2026-03-07 18:38:52 +0530

    Add compareTriplets function for score comparison
    Implement compareTriplets function to compare scores.

---
+++ compareTriplets.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    alice = 0
+    bob = 0
+
+    for i in range(3):
+        if a[i] > b[i]:
+            alice += 1
+        elif a[i] < b[i]:
+            bob += 1
+
+    return [alice, bob]
+
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+result = compareTriplets(a, b)
+print(result[0], result[1])
+
